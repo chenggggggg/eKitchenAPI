4a31feb93e5f12c5d9548fd11ef2625628b0ff71
Cheng <calvindemeester@gmail.com>
2020-11-22 22:42:16 +0100

    API uploaded

---
+++ iChefWebApi/iChefWebApi/Controller/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Web.Http;
+using iChefWebApi.Configuration;
+using iChefWebApi.Models;
+using MySql.Data.MySqlClient;
+
+namespace iChefWebApi.Controller
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public int Login([FromBody] User user)
+        {
+            int response = 0;
+            using (MySqlConnection conn = WebApiConfig.DatabaseConnection())
+            {
+                using (MySqlCommand cmd = new MySqlCommand("login_user", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("p_username", user.UserName);
+                    cmd.Parameters.AddWithValue("p_userpassword", user.Password);
+
+                    if (cmd.Connection.State != ConnectionState.Open)
+                        cmd.Connection.Open();
+                    using (MySqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        while(dataReader.Read())
+                        {
+                            response = (int)dataReader["user_id"];
+                        }
+                    }
+                }
+            }
+            return response;
+        }
+    }
+}
---
+++ iChefWebApi/iChefWebApi/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iChefWebApi.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public User(int userId)
+        {
+            UserId = userId;
+        }
+    }
+}
